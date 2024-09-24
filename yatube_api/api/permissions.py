0f4c120e4ada2556e70010aec8e7ce36b1a68f8e
Ilya <ilya.nizhellskiy@gmail.com>
2024-09-24 10:14:37 +0300

    0.6

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 
 class AuthorAccessPermission(permissions.BasePermission):
-    message = 'Удаление чужого контента запрещено!'
+    message = 'Изменение/удаление чужого контента запрещено!'
 
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
+        return request.method == 'GET' or obj.author == request.user
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -7,13 +7,10 @@ from api.views import CommentViewSet, GroupViewSet, PostViewSet
 v1_router = DefaultRouter()
 v1_router.register('posts', PostViewSet)
 v1_router.register('groups', GroupViewSet)
-
-v1_comment_router = DefaultRouter()
-v1_comment_router.register('comments', CommentViewSet)
+v1_router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet)
 
 
 urlpatterns = [
     path('v1/api-token-auth/', obtain_auth_token),
     path('v1/', include(v1_router.urls)),
-    path('v1/posts/<int:post_id>/', include(v1_comment_router.urls)),
 ]
